8feb13238521a3bba2c4167c0ec2d6504e0a7384
say2806 <sayma.gomez278978@potros.itson.edu.mx>
2026-01-29 20:32:48 -0700

    Add files via upload

---
+++ trabajo_mateintrod.py
@@ -0,0 +1,15 @@
+a = 415  #alumn
+
+ai = 221 # alum q estudian ingl
+
+af = 163 # alum q estudian fran
+
+aif = 52 #alumn q estudian ingl y fran
+
+uni = ai + af - aif # alumn q estudian ingl o franc
+
+an = a - uni #alum q no estudian ninguno
+
+print("Alumnos que no estudian ningun idioma:", an)
+print("Alumnos que estudian ingles o frances:", uni)
+print("Alumnos totales:", a)
